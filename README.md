231fb53560fb40f24a188def1a11ca80d5d92326
jicodes <liu.ji1@northeastern.edu>
2024-03-06 15:57:31 -0800

    add redis as db for the app

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Microservice with Go, Chi and Redis
+# Microservice with Go, Chi, Redis and Docker
+
+## Install Redis
+
+```sh
+brew install redis
+```
+
+## Install go-redis
+
+```sh
+go get github.com/redis/go-redis/v9
+```
+
+## Run Redis with Docker
+
+```sh
+docker run -d -p 6379:6379 --name redis redis:alpine
+```
--- application/app.go
+++ application/app.go
@@ -4,21 +4,33 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+
+	"github.com/redis/go-redis/v9"
 )
 
 type App struct {
 	router http.Handler
+	rdb *redis.Client
 }
 
 func New() *App {
 	app := &App{
 		router: loadRoutes(),
+		rdb: redis.NewClient(&redis.Options{}),
 	}
 	return app
 }
 
 func (app *App) Start(ctx context.Context) error {
-	err := http.ListenAndServe(":8080" , app.router)
+
+	err := app.rdb.Ping(ctx).Err()
+	if err != nil {
+		return fmt.Errorf("failed to ping redis: %w", err)
+	}
+
+	fmt.Println("Starting the server on :8080")
+
+	err = http.ListenAndServe(":8080" , app.router)
 	if err != nil {
 		return fmt.Errorf("failed to start server: %w", err)
 	}
