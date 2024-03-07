7ee098cc933d16617e6a64f32f709366f8513e22
jicodes <liu.ji1@northeastern.edu>
2024-03-06 19:51:29 -0800

    feat:implement graceful shutdown with goroutine/channel and signal/context packages

--- README.md
+++ README.md
@@ -17,3 +17,13 @@ go get github.com/redis/go-redis/v9
 ```sh
 docker run -d -p 6379:6379 --name redis redis:alpine
 ```
+
+## Graceful Shutdown or Graceful Termination
+
+- The application should be able to handle the termination signal and gracefully shutdown the server.
+- To handle the termination signal.
+  - run the server in `goroutine` so it won't block main thread.
+  - make a `chan` to communicate between the main goroutine and the server goroutine.
+  - in the main goroutine, listen for the termination signal and send the signal to the server goroutine.
+  - `os/signal` package is used to handle the termination signal.
+  - `context` package is used to communicate and handle gracefully shutdown of the server.
--- application/app.go
+++ application/app.go
@@ -4,36 +4,60 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 )
 
 type App struct {
 	router http.Handler
-	rdb *redis.Client
+	rdb    *redis.Client
+	server *http.Server
 }
 
 func New() *App {
 	app := &App{
 		router: loadRoutes(),
-		rdb: redis.NewClient(&redis.Options{}),
+		rdb:    redis.NewClient(&redis.Options{}),
 	}
 	return app
 }
 
 func (app *App) Start(ctx context.Context) error {
-
 	err := app.rdb.Ping(ctx).Err()
 	if err != nil {
 		return fmt.Errorf("failed to ping redis: %w", err)
 	}
 
-	fmt.Println("Starting the server on :8080")
+	defer func() {
+		if err := app.rdb.Close(); err != nil {
+			fmt.Println("failed to close redis connection:", err)
+		}
+	}()
 
-	err = http.ListenAndServe(":8080" , app.router)
-	if err != nil {
-		return fmt.Errorf("failed to start server: %w", err)
+	fmt.Println("Starting the server on :8080")
+	app.server = &http.Server{
+		Addr:    ":8080",
+		Handler: app.router,
 	}
 
-	return nil
+	ch := make(chan error, 1)
+
+	go func() {
+		err = app.server.ListenAndServe()
+		if err != nil {
+			ch <- fmt.Errorf("failed to start server: %w", err)
+		} 
+		close(ch)
+	}()
+
+	select {
+	case err = <-ch:
+		return err
+	case <-ctx.Done():
+		timeout, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		app.server.Shutdown(timeout)
+		return nil
+	}
 }
